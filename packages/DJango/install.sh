a742b6b63b6a1657aac1dc84f76f52fbd52bc92a
root <root@lcc2-15.lcc.ufcg.edu.br>
2017-04-26 16:51:30 -0300

    Django

---
+++ packages/DJango/install.sh
@@ -0,0 +1,17 @@
+#################################################################################################
+#                                                                                                                                                                                       #
+# Script de instalaço  Django                                                           
+                                                     # #
+# Autores: Ana Godoy - godoyisadora@gmail.com      #
+# Abril de 2017                                                                                                                                                       #
+#                                                                                                                                                                                       #
+#################################################################################################
+
+#Update
+apt-get -y update
+
+#Install Django
+apt-get -y install python-django
+
+
+
