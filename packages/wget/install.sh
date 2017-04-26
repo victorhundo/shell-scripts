113ddd35a757c4fa34915da0773490e0531a8134
root <root@lcc2-14.lcc.ufcg.edu.br>
2017-04-26 15:47:25 -0300

    Adiciona o script do wget

---
+++ packages/wget/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##########################################################################                                                                                                                                   #
+# Script de instalação dwget                                                 #
+# Autores: Olavo Brito - olavo.souza@ccc.ufcg.edu.br                           #
+# abril de 2017                                                                                                       #
+#                                                                                                                                   #
+#########################################################################
+
+
+#atualiza os apt-get
+apt-get update
+
+#instala wget
+apt-get install wget
