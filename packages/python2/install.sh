78b86444856def9b8866a07f98003d4f2364a633
root <root@lcc2-15.lcc.ufcg.edu.br>
2017-04-26 15:41:13 -0300

    Install Python 2.7

---
+++ packages/python2/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################################################################################################
+#                                                                                                                                                                                       #
+# Script de instalaçã Python 2                                                                                                                        # #
+# Autores: Ana Godoy - godoyisadora@gmail.com      #
+# marco de 2017                                                                                                                                                       #
+#                                                                                                                                                                                       #
+#################################################################################################
+
+#Update
+apt-get update
+
+#Install Python 2.7
+apt-get install -y python2.7
+
